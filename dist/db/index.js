f825e1422db51e56529141a5b0c07744ada088fb
Emmanuel Guerreiro <emmanuelguerreiro98@gmail.com>
2025-05-18 23:48:17 -0300

    udpated docker ignore and gitignore

--- dist/db/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-require("dotenv/config");
-const node_postgres_1 = require("drizzle-orm/node-postgres");
-if (!process.env.DATABASE_URL) {
-    throw new Error('No database_url implemented in .env');
-}
-const db = (0, node_postgres_1.drizzle)(process.env.DATABASE_URL);
